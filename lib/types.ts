5a7b28422d146ed738fe720a0e1144b3f78057ba
Suhanur Rahman <suhanurrahman.r@gmail.com>
2026-09-13 02:04:02 +0600

    Add content format type

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,3 @@
-export type Post={id:string;title:string;slug:string;excerpt:string|null;content:string;coverImage:string|null;status:'DRAFT'|'PUBLISHED';publishedAt:string|null;createdAt:string;updatedAt:string;categoryId:string|null;categoryName?:string|null;categorySlug?:string|null;seoTitle?:string|null;seoDescription?:string|null;seoKeywords?:string|null;ogImage?:string|null;canonicalUrl?:string|null;noindex?:number}
+export type Post={id:string;title:string;slug:string;excerpt:string|null;content:string;contentFormat:'HTML'|'MARKDOWN';coverImage:string|null;status:'DRAFT'|'PUBLISHED';publishedAt:string|null;createdAt:string;updatedAt:string;categoryId:string|null;categoryName?:string|null;categorySlug?:string|null;seoTitle?:string|null;seoDescription?:string|null;seoKeywords?:string|null;ogImage?:string|null;canonicalUrl?:string|null;noindex?:number}
 export type Category={id:string;name:string;slug:string}
 export type Tag={id:string;name:string;slug:string}
