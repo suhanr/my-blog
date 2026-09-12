25749538663487ebdb0c5d98b7d4526692b4284a
Suhanur Rahman <suhanurrahman.r@gmail.com>
2026-09-12 22:35:55 +0600

    Merge unpublished and special WordPress records into migration snapshot

---
+++ scripts/merge-wordpress-supplement.mjs
@@ -0,0 +1,24 @@
+import fs from 'node:fs/promises'
+
+const mainPath = 'migration-data/wordpress.json'
+const supplementPath = 'migration-data/wordpress-supplement.json'
+
+const main = JSON.parse(await fs.readFile(mainPath, 'utf8'))
+const supplement = JSON.parse(await fs.readFile(supplementPath, 'utf8'))
+
+const existingPosts = new Map(main.posts.map((post) => [String(post.id), post]))
+for (const post of supplement.posts || []) {
+  if (!existingPosts.has(String(post.id))) existingPosts.set(String(post.id), post)
+}
+
+const existingComments = new Map(main.comments.map((comment) => [String(comment.id), comment]))
+for (const comment of supplement.comments || []) {
+  if (!existingComments.has(String(comment.id))) existingComments.set(String(comment.id), comment)
+}
+
+main.posts = [...existingPosts.values()]
+main.comments = [...existingComments.values()]
+main.mergedSupplement = true
+await fs.writeFile(mainPath, JSON.stringify(main, null, 2))
+
+console.log(`Merged WordPress snapshot: ${main.posts.length} posts and ${main.comments.length} comments.`)
