77906df46a6ea866367c2b944a3f73cfb4cd9cd9
Suhanur Rahman <suhanurrahman.r@gmail.com>
2026-09-14 04:43:29 +0600

    Connect search page to URL queries

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -10,11 +10,11 @@ export const metadata: Metadata = {
   alternates: { canonical: 'https://blog.suhanurrahman.com/search/' },
 }
 
-export default async function SearchPage() {
-  const [categories, menu] = await Promise.all([getCategories(), getHeaderMenu()])
+export default async function SearchPage({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
+  const [{ q = '' }, categories, menu] = await Promise.all([searchParams, getCategories(), getHeaderMenu()])
   return (
     <div className="site-public">
-      <SearchPageClient categories={categories} menu={menu} />
+      <SearchPageClient categories={categories} menu={menu} initialQuery={q} />
     </div>
   )
 }
